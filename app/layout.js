5026f1605c7b27e2ef2fd2894de9afbeb539c8c9
sebstonge <sebstonge@gmail.com>
2026-05-05 20:05:06 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Pool NFL",
+  description: "Application de pool NFL",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
