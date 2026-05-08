69698957f2546c154dfb151a6401ae464491b35c
sebstonge <sebstonge@gmail.com>
2026-05-08 13:54:15 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
-import BottomNav from "../components/BottomNav";
+import BottomNav from "./components/BottomNav";
 
 function NavItem({ href, icon, title, subtitle, color }) {
   return (
