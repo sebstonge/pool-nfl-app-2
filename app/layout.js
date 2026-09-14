d16ec58c4d65cdf7233e99c43721c3fd4f0e1372
sebstonge <sebstonge@gmail.com>
2026-09-14 11:28:44 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,23 @@ import "./globals.css";
 export const metadata = {
   title: "Pool NFL",
   description: "Application de pool NFL",
+
+  manifest: "/manifest.json",
+
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Pool NFL",
+  },
 };
 
-export default function RootLayout({ children }) {
+export const viewport = {
+  themeColor: "#020617",
+};
+
+export default function RootLayout({
+  children,
+}) {
   return (
     <html lang="fr">
       <body>{children}</body>
